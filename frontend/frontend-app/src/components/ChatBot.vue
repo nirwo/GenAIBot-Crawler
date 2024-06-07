24c9f2a56a3a9830dba58a2f00f3e96e254be5e4
nirwo <wolffdesign@gmail.com>
2024-06-08 01:11:48 +0300

    Update ChatBot.vue
    enhancments

--- frontend/frontend-app/src/components/ChatBot.vue
+++ frontend/frontend-app/src/components/ChatBot.vue
@@ -136,14 +136,14 @@ export default {
     isValidUrl(url) {
       const pattern = new RegExp(
         "^(https?:\\/\\/)?" + // protocol
-          "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-          "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-          "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-          "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-          "(\\#[-a-z\\d_]*)?$",
-        "i"
-      ); // fragment locator
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$", "i" // fragment locator
+      );
       return !!pattern.test(url);
+    },
     },
     escapeRegExp(string) {
       return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
